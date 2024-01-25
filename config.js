138e94d8d74bba8a1a9150283e8b7f3ebae1b335
aryanbisht2580 <aryanbisht2580@gmail.com>
2024-01-25 21:04:22 +0530

    update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connect=async()=>{
-    await mongoose.connect("mongodb+srv://aryanbisht2580:aryan2534@cluster0.ldwshl4.mongodb.net/?retryWrites=true&w=majority&appName=chatapp",{
+    await mongoose.connect("mongodb+srv://aryanbisht2580:aryan2534@cluster0.ldwshl4.mongodb.net/chatapp?retryWrites=true&w=majority",{
         useNewUrlParser:true,
         useUnifiedTopology:true
     })
